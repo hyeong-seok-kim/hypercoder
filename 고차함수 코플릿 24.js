dbb8ade7a5384d951bc41fc010d66cb964207955
hyeong-seok-kim <dream64144@gmail.com>
2022-04-26 22:38:57 +0900

    고차함수_코플릿23,24

---
+++ 고차함수 코플릿 24.js
@@ -0,0 +1,23 @@
+// 문제
+// number 타입을 요소로 갖는 배열을 입력받아 배열의 모든 요소의 곱을 리턴해야 합니다.
+
+// 입력
+// 인자 1 : arr
+// number 타입을 요소로 갖는 배열
+
+// 출력
+// number 타입을 리턴해야 합니다.
+
+// 주의 사항
+// 반복문(for, while) 사용은 금지됩니다.
+// 반드시 arr.reduce를 이용해서 풀어야 합니다.
+// 빈 배열을 입력받은 경우, 1을 리턴해야 합니다.
+
+function computeProductOfAllElements(arr) {
+  return arr.reduce(function(i,k){
+    return i * k
+  },1)
+}
+
+let output = computeProductOfAllElements([2, 5, 6]);
+console.log(output); // --> 60
